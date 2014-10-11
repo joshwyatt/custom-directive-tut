66bf373e84f4b48584919b53fcdf5c78eb4504eb
joshWyatt <joshuawyatt123@gmail.com>
2014-10-10 17:12:48 -0700

    Write color changing directive

--- controllers.js
+++ controllers.js
@@ -4,6 +4,16 @@ function TestController(){
   vm.hello = 'hello';
 }
 
+function MainController($scope){
+  
+}
+
+function OtherController($scope){
+
+}
+
 angular.module('controllers', [])
 
 .controller('TestController', TestController)
+.controller('MainController', MainController)
+.controller('OtherController', OtherController)
--- directives.js
+++ directives.js
@@ -7,10 +7,27 @@ function helloWorld(){
         'Hello World',
       '</pre>'
     ].join('')
-  }
+  };
+}
+
+function colorWorld(){
+  return {
+    restrict: 'EA',
+    replace: 'true',
+    template: '<p style="background-color:{{color}}">Color World</p>',
+    link: function(scope, elem, attrs){
+      elem.bind('click', function(){
+        elem.css('background-color', 'white');
+        scope.$apply(function(){
+          scope.color = "white";
+        });
+      });
+    }
+  };
 }
 
 
 angular.module('directives', [])
 
 .directive('helloWorld', helloWorld)
+.directive('colorWorld', colorWorld)
